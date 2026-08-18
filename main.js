c4cca68bf874ce32dfd8ebdfeeaf68ccaf0ad399
Vimnm <vimnm2259@gmail.com>
2026-08-18 10:14:56 +0500

    final_product

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
-[
-    {
-        user:"vimnm"
-        city:"not found"
-        address:"not found"
-    }
-]
+fetch("https://raw.githubusercontent.com/vimnm2259-hue/first_api/refs/heads/main/main.json")
+
+    .then(response => {
+        if (!response.ok) {
+            throw new Error("Ошыбка сервера")
+        };
+        return response.json()
+    })
+    .then(data => {
+        console.log("вот данные: ", data);
+    })
+    .catch(error => {
+        console.error(error);
+    })
